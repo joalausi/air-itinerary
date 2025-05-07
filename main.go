e79f2de62d46da55bcb7c63cbbd1c6784dda55d6
joalausi <artchornonog@gmail.com>
2025-05-07 23:37:23 +0300

    stable

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ func main() {
 	flag.Usage = func() {
 		fmt.Println(utls.Yellow + "Usage: go run . <input.txt> <output.txt> <airport-lookup.csv>" + utls.Reset)
 	}
-	// Разбираем аргументы командной строки
-	flag.Parse()
-
-	if flag.NArg() < 3 {
-		flag.Usage()
-		os.Exit(1)
-	}
+	
+	// Parse command-line arguments
+    flag.Parse()
+    if flag.NArg() < 3 {
+        flag.Usage()
+        os.Exit(1)
+    }
 
 	// // Флаг для списка вещей в поездку
 	// packingListFlag := flag.Bool("packing-list", false, "Displays a suggested packing list")
@@ -34,24 +34,11 @@ func main() {
 	// 	return
 	// }
 
-	// args := flag.Args()
-	// if len(args) != 3 { // Ожидаем три аргумента
-	// 	flag.Usage()
-	// 	os.Exit(1)
-	// }
 
 	// Parse command-line arguments
-	inputFile := flag.Arg(0)
-	outputFile := flag.Arg(1)
-	lookupFile := flag.Arg(2)
-
-	// // Чтение входного файла
-	// inputData, err := utls.ReadFile(inputFile)
-	// if err != nil {
-	// 	fmt.Printf(utls.Red + "Ошибка при чтении входного файла: %v\n", err, utls.Reset)
-	// 	os.Exit(1)
-	// 	return
-	// }
+	inputFile := flag.Arg(0)    // Path to input text file
+    outputFile := flag.Arg(1)   // Path to output text file (to be created)
+    lookupFile := flag.Arg(2)   // Path to airport lookup CSV file
 
 	// // Здесь можно добавить обработку inputData, работу с outputPath и lookupPath
 
@@ -67,6 +54,15 @@ func main() {
 	if err != nil {
 		fmt.Printf(utls.Red+"Error loading airport lookup: %v\n"+utls.Reset, err)
 		os.Exit(1)
+	    // lookupData = map[string]string{
+        // "LAX":  "Los Angeles International Airport",
+        // "EGLL": "London Heathrow Airport",
+        // "HAJ":  "Hannover Airport",
+        // "EDDW": "Bremen Airport",
+        // "CEG":  "Hawarden Airport",
+        // "WWK":  "Wewak International Airport",
+        // "BIH":  "Eastern Sierra Regional Airport",
+        // }
 	}
 
 	// parse input data
@@ -106,4 +102,61 @@ func WriteToFile(outputFile string, data []string) error {
 		}
 	}
 	return nil
+}
+
+    // Read input file (each line as an element in the slice)
+    inputData, err := utls.ReadFile(inputFile)
+    if err != nil {
+        fmt.Printf(utls.Red+"Error reading input file %s: %v\n"+utls.Reset, inputFile, err)
+        os.Exit(1)
+    }
+
+    // Load airport data from CSV file into a map (code -> full name)
+    lookupData, err := utls.LoadAirportData(lookupFile)
+    if err != nil {
+        // If loading from CSV fails (function not implemented or file missing), use a fallback map
+        fmt.Println(utls.Yellow + "Warning: using default airport data map (limited) due to error:", err, utls.Reset)
+    }
+
+    // Parse the input lines into a list of Flight structures
+    flights, err := parser.Parse(inputData, lookupData)
+    if err != nil {
+        fmt.Printf(utls.Red+"Error parsing data: %v\n"+utls.Reset, err)
+        os.Exit(1)
+    }
+
+    // Format the list of Flight structures into output lines
+    formattedData, err := formatter.Format(flights, lookupData)
+    if err != nil {
+        fmt.Printf(utls.Red+"Error formatting data: %v\n"+utls.Reset, err)
+        os.Exit(1)
+    }
+
+    // Write the formatted output lines to the output file
+    err = WriteToFile(outputFile, formattedData)
+    if err != nil {
+        fmt.Printf(utls.Red+"Error: Failed to write to file %s: %v\n"+utls.Reset, outputFile, err)
+        os.Exit(1)
+    }
+
+    // If we reached here, everything succeeded
+    fmt.Println(utls.Blue + "Success: output written to " + outputFile + utls.Reset)
+}
+
+// WriteToFile writes the slice of strings to the specified output file, one line at a time.
+func WriteToFile(filePath string, lines []string) error {
+    file, err := os.Create(filePath)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    // Write each line followed by a newline
+    for _, line := range lines {
+        _, err := file.WriteString(line + "\n")
+        if err != nil {
+            return err
+        }
+    }
+    return nil
 }
